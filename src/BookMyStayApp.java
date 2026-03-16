9f315ff47d2edd25e4f11c7084ca53d70582d531
Jashwanth <jash29102006@gmail.com>
2026-03-16 08:41:43 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,6 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay App!");
+        // ...future use cases will be added here...
+    }
+}
